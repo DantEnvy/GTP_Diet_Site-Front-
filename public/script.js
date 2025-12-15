75b00b95c34f54ecfafa744c32d90e1cd2e4b587
DantEnvy <lukashivarsenii@gmail.com>
2025-12-15 20:35:19 +0200

    123

---
+++ public/script.js
@@ -0,0 +1,20 @@
+async function send() {
+  const data = {
+    carb: document.getElementById("carbs").value,
+    bmr: document.getElementById("bmr_last").value,
+    fat: document.getElementById("fats").value,
+    squirrels: document.getElementById("squirrel").value,
+    allergy: document.getElementById("allergy").value,
+    health: document.getElementById("health").value
+  };
+  
+  const res = await fetch("/api/diet", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data)
+  });
+
+  const result = await res.json();
+  document.getElementById("result").innerText = result.diet;
+}
+
